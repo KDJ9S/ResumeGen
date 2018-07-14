8a3ef977b4a594149dbb9ed0dae8f8230cf99a32
Sugar <406857586@qq.com>
2018-07-14 17:14:28 +0800

    增加 简历模板 页面

---
+++ src/main/java/team/ftg/resumegen/controller/TemplateController.java
@@ -0,0 +1,14 @@
+package team.ftg.resumegen.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("user")
+public class TemplateController {
+
+    @RequestMapping("/template")
+    public String template() { return "template.jsp";}
+}
