d6d64e34bb10606a69632e35caa26421c118692a
Sugar <406857586@qq.com>
2018-07-20 16:08:04 +0800

    db

--- src/main/java/team/ftg/resumegen/db/db.sql
+++ src/main/java/team/ftg/resumegen/db/db.sql
@@ -1,78 +1,21 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : Conn
+ Source Server         : test
  Source Server Type    : MySQL
- Source Server Version : 80011
+ Source Server Version : 50721
  Source Host           : localhost:3306
  Source Schema         : testdb
 
  Target Server Type    : MySQL
- Target Server Version : 80011
+ Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 16/07/2018 19:03:21
+ Date: 20/07/2018 16:06:53
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for basic_info
--- ----------------------------
-DROP TABLE IF EXISTS `basic_info`;
-CREATE TABLE `basic_info` (
-  `basic_id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `gender` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `age` int(10) DEFAULT NULL,
-  `nation` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `city` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `tel` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `email` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `school` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `major` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `education` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `user_id` int(20) NOT NULL,
-  PRIMARY KEY (`basic_id`,`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Table structure for exp_info
--- ----------------------------
-DROP TABLE IF EXISTS `exp_info`;
-CREATE TABLE `exp_info` (
-  `exp_id` int(20) NOT NULL AUTO_INCREMENT,
-  `edu_exp` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `project_exp` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `user_id` int(20) NOT NULL,
-  PRIMARY KEY (`exp_id`,`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Table structure for intent_info
--- ----------------------------
-DROP TABLE IF EXISTS `intent_info`;
-CREATE TABLE `intent_info` (
-  `intent_id` int(20) NOT NULL AUTO_INCREMENT,
-  `position` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `job_type` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `work_city` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `salary` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `user_id` int(20) NOT NULL,
-  PRIMARY KEY (`intent_id`,`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-
--- ----------------------------
--- Table structure for intro_info
--- ----------------------------
-DROP TABLE IF EXISTS `intro_info`;
-CREATE TABLE `intro_info` (
-  `intro_id` int(20) NOT NULL AUTO_INCREMENT,
-  `introduce` text CHARACTER SET utf8 COLLATE utf8_general_ci,
-  `user_id` int(20) NOT NULL,
-  PRIMARY KEY (`intro_id`,`user_id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for User
@@ -83,7 +26,65 @@ CREATE TABLE `User` (
   `username` text,
   `password` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=124 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for basic_info
+-- ----------------------------
+DROP TABLE IF EXISTS `basic_info`;
+CREATE TABLE `basic_info` (
+  `basic_id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` text CHARACTER SET utf8,
+  `gender` text CHARACTER SET utf8,
+  `age` int(10) DEFAULT NULL,
+  `nation` text CHARACTER SET utf8,
+  `city` text CHARACTER SET utf8,
+  `tel` text CHARACTER SET utf8,
+  `email` text CHARACTER SET utf8,
+  `school` text CHARACTER SET utf8,
+  `major` text CHARACTER SET utf8,
+  `education` text CHARACTER SET utf8,
+  `image` text,
+  `user_id` int(20) NOT NULL,
+  PRIMARY KEY (`basic_id`,`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for exp_info
+-- ----------------------------
+DROP TABLE IF EXISTS `exp_info`;
+CREATE TABLE `exp_info` (
+  `exp_id` int(20) NOT NULL AUTO_INCREMENT,
+  `edu_exp` text CHARACTER SET utf8,
+  `project_exp` text CHARACTER SET utf8,
+  `user_id` int(20) NOT NULL,
+  PRIMARY KEY (`exp_id`,`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for intent_info
+-- ----------------------------
+DROP TABLE IF EXISTS `intent_info`;
+CREATE TABLE `intent_info` (
+  `intent_id` int(20) NOT NULL AUTO_INCREMENT,
+  `position` text CHARACTER SET utf8,
+  `job_type` text CHARACTER SET utf8,
+  `work_city` text CHARACTER SET utf8,
+  `salary` text CHARACTER SET utf8,
+  `user_id` int(20) NOT NULL,
+  PRIMARY KEY (`intent_id`,`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for intro_info
+-- ----------------------------
+DROP TABLE IF EXISTS `intro_info`;
+CREATE TABLE `intro_info` (
+  `intro_id` int(20) NOT NULL AUTO_INCREMENT,
+  `introduce` text CHARACTER SET utf8,
+  `user_id` int(20) NOT NULL,
+  PRIMARY KEY (`intro_id`,`user_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Table structure for user_resume
@@ -94,6 +95,6 @@ CREATE TABLE `user_resume` (
   `user_id` int(11) DEFAULT NULL,
   `resume_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
